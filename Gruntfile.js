47c43552563c1ad464fed19ed7201d0fee1ce3cc
Alan Brian Dardic <alanbd@wixs-MacBook-Pro-38.local>
2015-07-01 12:26:52 +0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+// Generated on 2015-07-01 using generator-wix-angular 0.1.130
+'use strict';
+
+module.exports = function (grunt) {
+  require('wix-gruntfile')(grunt, {
+    cdnify: 'vm',
+    port: 9000,
+    preloadModule: 'firstWixAngularPreload',
+    translationsModule: 'firstWixAngularTranslations',
+    svgFontName: 'first-wix-angular',
+    karmaConf: require('./karma.conf.js'),
+    protractor: true,
+    bowerComponent: true
+  });
+
+  grunt.modifyTask('yeoman', {
+    //the address to which your local /_api is proxied to (to workaround CORS issues)
+    api: 'http://www.pizza.wixpress.com/_api/',
+    //api: 'http://localhost:3000',
+
+    //this is the node.js fake server that e2e tests will use
+    e2eTestServer: 'http://localhost:3333/',
+
+    //the address that opens in your browser in grunt serve
+    //(domain should be the same as staging so cookies will be sent in api requests)
+    local: 'http://local.pizza.wixpress.com:<%= connect.options.port %>/'
+  });
+
+  //override sauce labs browser list
+  //process.env.SAUCE_BROWSERS = 'Chrome FF';
+
+  try {
+    require('./Gruntfile.private.js')(grunt); //override stuff locally
+  } catch (err) {
+    if (err.code !== 'MODULE_NOT_FOUND') {
+      throw (err);
+    }
+  }
+
+  //Follow this URL for instructions on how to override built-in definitions:
+  //https://github.com/wix/wix-gruntfile/blob/master/README.md
+};
